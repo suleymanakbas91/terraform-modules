b54d498e334d80eb4cf8b8d6981f1db3037b6502
I332735 <suleyman.akbas@sap.com>
2020-07-30 11:36:28 +0200

    update

--- azurerm_kubernetes_cluster/main.tf
+++ azurerm_kubernetes_cluster/main.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
-  version         = "~>2.20"
+  version         = "~>2.0.0"
   subscription_id = var.subscription_id
   client_id       = var.client_id
   client_secret   = var.client_secret
